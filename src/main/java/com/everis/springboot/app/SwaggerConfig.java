94ec4457e6757430431c8958185e9089c9479875
rimegaray <rimegaraysanmarcos@gmail.com>
2019-11-28 15:39:01 -0500

    Swagger

---
+++ src/main/java/com/everis/springboot/app/SwaggerConfig.java
@@ -0,0 +1,47 @@
+package com.everis.springboot.app;
+
+import java.time.LocalDateTime;
+import java.util.Date;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2WebFlux;
+
+@Configuration
+@EnableSwagger2WebFlux
+public class SwaggerConfig {
+  /**
+
+   * Esto sirve para borrar los registros de la base de datos y volver
+   * a crear 5 datos de prueba.
+
+   */
+
+  @Bean
+  public Docket newsApi() {
+    return new Docket(DocumentationType.SWAGGER_2)
+                .groupName("api-Cursos")
+                .apiInfo(apiInfo())
+                .directModelSubstitute(LocalDateTime.class, Date.class)
+                .select()
+                .paths(PathSelectors.regex("/api.*"))
+                .build();
+  }
+ 
+  private ApiInfo apiInfo() {
+    return new ApiInfoBuilder()
+                .title("Infos REST api")
+                .description("PoC of a REST api, Cursos")
+                .termsOfServiceUrl("http://en.wikipedia.org/wiki/Terms_of_service")
+                .license("Apache License Version 2.0")
+                .licenseUrl("http://www.apache.org/licenses/LICENSE-2.0.html")
+                .version("2.0")
+                .build();
+  }
+}
--- src/main/java/com/everis/springboot/app/controllers/CourseController.java
+++ src/main/java/com/everis/springboot/app/controllers/CourseController.java
@@ -18,26 +18,46 @@ import com.everis.springboot.app.models.documents.Course;
 import com.everis.springboot.app.models.documents.Student;
 import com.everis.springboot.app.models.service.CourseService;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 @RestController
 @RequestMapping("/api/course")
+@Api(value = "course", description = "API para curso", produces = "application/json")
 public class CourseController {
 
 	@Autowired
 	private CourseService service;
 
+	 @ApiOperation(value = "Listar todos los cursos", 
+		      notes = "Retorna todo sobre todas los cursos")
+	 @ApiResponses({
+	      @ApiResponse(code = 200, message = "Encuentra por lo menos un curso")
+	    })
 	@GetMapping
 	public Flux<Course> findAll() {
 		return service.findAllCourses();
 	}
-
+	 
+	@ApiOperation(value = "Busca curso por id", 
+		      notes = "Retorna todo sobre un curso")
+	@ApiResponses({
+	      @ApiResponse(code = 200, message = "Encuentra por lo menos un curso")
+	    })
 	@GetMapping("/{id}")
 	public Mono<Course> findById(@PathVariable String id) {
 		return service.findByIdCourse(id);
 	}
 
+	@ApiOperation(value = "Crear un nuevo curso", 
+		      notes = "Retorna el curso creado")
+	@ApiResponses({
+	      @ApiResponse(code = 200, message = "Curso creado")
+	    })
 	@PostMapping
 	@ResponseStatus(HttpStatus.CREATED)
 	public Mono<Course> create(@RequestBody Course course) {
@@ -53,6 +73,11 @@ public class CourseController {
 
 	}
 
+	@ApiOperation(value = "Actualizar un curso", 
+		      notes = "Retorna el curso actualizado")
+	@ApiResponses({
+	      @ApiResponse(code = 200, message = "Curso actualizado")
+	    })
 	@PutMapping("/{id}")
 	public Mono<Course> update(@RequestBody Course course, @PathVariable String id){
 		
@@ -67,12 +92,22 @@ public class CourseController {
 		
 	}
 
+	@ApiOperation(value = "Eliminar un curso", 
+		      notes = "Retorna void")
+	@ApiResponses({
+	      @ApiResponse(code = 200, message = "Se elimina el curso")
+	    })
 	@DeleteMapping("/{id}")
 	@ResponseStatus(HttpStatus.NO_CONTENT)
 	public Mono<Void> delete(@PathVariable String id){
 		return service.findByIdCourse(id).flatMap(s->service.deleteCourse(s));
 	}
 	
+	@ApiOperation(value = "Agregar un estudiante a un curso abierto", 
+		      notes = "Retorna todo sobre el curso")
+	@ApiResponses({
+	      @ApiResponse(code = 200, message = "se agrego al estudiante")
+	    })
 	@PutMapping("/addStudent/{idCourse}/{idStudent}")
 	public Mono<Course> addStudent(@PathVariable String idCourse, @PathVariable String idStudent) {
 		return service.findByIdCourse(idCourse).flatMap(c->{
@@ -90,6 +125,11 @@ public class CourseController {
 		});
 	}
 
+	@ApiOperation(value = "Agregar familiares a un nuevo curso", 
+		      notes = "Retorna todo sobre el curso")
+	@ApiResponses({
+	      @ApiResponse(code = 200, message = "se agrego al familiar ")
+	    })
 	@PutMapping("/addPeople/{idCourse}/{idPerson}")
 	public Mono<Course> addPeople(@PathVariable String idCourse, @PathVariable String idPerson) {
 		return service.findAllCourses().filter(c->c.getIdPersons().contains(idPerson)).count()
@@ -114,31 +154,61 @@ public class CourseController {
 				});
 	}
 	
+	@ApiOperation(value = "Listar todos los cursos que lleva un familiar", 
+		      notes = "Retorna todo sobre todas los cursos")
+	@ApiResponses({
+	      @ApiResponse(code = 200, message = "Encuentra por lo menos un curso")
+	    })
 	@GetMapping("/coursesPerson/{idPerson}")
 	public Flux<Course> findCoursesByIdPerson(@PathVariable String idPerson){
 		return service.findAllCourses().filter(c->c.getIdPersons().contains(idPerson));
 	}
 	
+	@ApiOperation(value = "Listar todos los cursos que llva un estudiante", 
+		      notes = "Retorna todo sobre todas los cursos")
+	@ApiResponses({
+	      @ApiResponse(code = 200, message = "Encuentra por lo menos un curso")
+	    })
 	@GetMapping("/coursesStudent/{idStudent}")
 	public Flux<Course> findCoursesByIdStudent(@PathVariable String idStudent){
 		return service.findAllCourses().filter(c->c.getIdStudents().contains(idStudent));
 	}
 	
+	@ApiOperation(value = "Listar todos los cursos que dicta un profesor", 
+		      notes = "Retorna todo sobre todas los cursos")
+	@ApiResponses({
+	      @ApiResponse(code = 200, message = "Encuentra por lo menos un curso")
+	    })
 	@GetMapping("/coursesTeacher/{idTeacher}")
 	public Flux<Course> findCoursesByIdTeacher(@PathVariable String idTeacher){
 		return service.findAllCourses().filter(c->c.getIdTeacher().contains(idTeacher));
 	}
 	
+	@ApiOperation(value = "Listar todos los cursos abiertos", 
+		      notes = "Retorna todo sobre todas los cursos")
+	@ApiResponses({
+	      @ApiResponse(code = 200, message = "Encuentra por lo menos un curso")
+	    })
 	@GetMapping("/openCourses")
 	public Flux<Course> findOpenCourses(){
 		return service.findAllCourses().filter(c->c.getState().equals("abierto"));
 	}
 	
+	@ApiOperation(value = "Listar todos los cursos activos", 
+		      notes = "Retorna todo sobre todas los cursos")
+	@ApiResponses({
+	      @ApiResponse(code = 200, message = "Encuentra por lo menos un curso")
+	    })
 	@GetMapping("/activeCourses")
 	public Flux<Course> findActiveCourses(){
 		return service.findAllCourses().filter(c->c.getState().equals("activo"));
 	}
 	
+	@ApiOperation(value = "Listar todos los estudiante de un curso", 
+		      notes = "Retorna todo sobre todas los cursos")
+	@ApiResponses({
+	      @ApiResponse(code = 200, message = "Encuentra por lo menos un curso")
+	    })
 	@GetMapping("/findStudents/{idCourse}")
 	public Flux<Student> findStudents(@PathVariable String idCourse){
 		return service.findStudents(idCourse);
